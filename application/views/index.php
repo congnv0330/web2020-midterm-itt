4e45249af57a7733e743fc210d3241826fa81be9
Nguyễn Văn Công <congnguyenlop9a1@gmail.com>
2020-07-04 08:53:52 +0700

    Add bootstrap, config base_url

---
+++ application/views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Trang chủ</title>
+	<link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+</head>
+<body>
+	<script src="<?= base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
+</body>
+</html>
