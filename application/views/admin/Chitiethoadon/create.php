9597f444e245118f4eb9e5c66ec532e271e58273
haovt0110 <4401104081@student.hcmup.edu.vn>
2020-07-04 10:50:11 +0700

    Tao view admin - Chuc nang Them chi tiet hoa don

--- application/views/admin/Chitiethoadon/create.php
+++ application/views/admin/Chitiethoadon/create.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Trang chủ</title>
+	<title>Tạo Chi tiết hóa đơn</title>
 	<link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
 </head>
 <body>
@@ -14,45 +14,35 @@
         <div class="card">
           <div class="card-body">
           <div class="text-left mb-3">
-            <a href="<?= base_url('index.php/admin/Hanghoa/index') ?>" id="btnBack" class="btn btn-primary">Quay về </a>
+            <a href="<?= base_url('index.php/admin/Chitiethoadon/index') ?>" id="btnBack" class="btn btn-primary">Quay về </a>
           </div>
-          <h4 class="card-title"><i class="mdi mdi-format-list-bulleted-type"></i> Thêm hàng hóa</h4>          
+          <h4 class="card-title"><i class="mdi mdi-format-list-bulleted-type text-center"></i> THÊM CHI TIẾT HÓA ĐƠN</h4>          
         </div>
         <div class="card-body">
             <form action="#" method="post">
                 <input type="hidden" name="_token" value="uRHGWpTT8FrXAx7rRoAwCdeWLhvhdFrwKRVYpUga">                
                 <div class="form-group row">
-                    <label for="code" class="col-sm-2 col-form-label">Mã hàng hóa</label>
+                    <label for="code" class="col-sm-2 col-form-label">Mã chi tiết hóa đơn</label>
                     <div class="col-sm-10">
-                      <input type="text" class="form-control " id="code" name="code" placeholder="Nhập mã" value="" required="">
+                      <input type="text" class="form-control " id="code" name="code1" placeholder="Nhập mã" value="" required="">
+                    </div>
+                </div>
+                 <div class="form-group row">
+                    <label for="code2" class="col-sm-2 col-form-label">Mã hóa đơn</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control " id="code2" name="code2" placeholder="Nhập mã" value="" required="">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="name" class="col-sm-2 col-form-label">Tên hàng hóa</label>
+                    <label for="code3" class="col-sm-2 col-form-label">Mã hàng hóa</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control " id="name" name="name" placeholder="Nhập tên ..." value="" required="">
+                      <input type="text" class="form-control " id="code3" name="code3" placeholder="Nhập mã" value="" required="">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="parent" class="col-md-2 col-form-label">Loại</label>
-                    <div class="col-md-10">
-                      <select name="role" id="role" class="custom-select ">
-                      </select>
-                  </div>
-                </div>
-                <div class="form-group row">
-                    <label for="price" class="col-sm-2 col-form-label">Đơn giá</label>
+                    <label for="soluong" class="col-sm-2 col-form-label">Số lượng</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control " id="price" name="price" placeholder="Nhập giá price vnđ..." value="" required="">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="image" class="col-sm-2 col-form-label">Hình</label>
-                    <div class="col-sm-10">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFileLang" lang="pl-Pl">
-                        <label class="custom-file-label" for="customFileLang">Chọn ảnh</label>
-                      </div>
+                        <input type="text" class="form-control" id="soluong" name="soluong" placeholder="Nhập số lượng..." value="" required="">
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Xác nhận</button>
