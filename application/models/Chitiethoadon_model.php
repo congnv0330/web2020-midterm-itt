29850c2f751ce6d3248e7ad5d7141cfd6ee43759
thaoptt1102 <thaoptt.cnthongtin@gmail.com>
2020-07-04 09:02:20 +0700

    Tao bang Hoa don va Chi tiet hoa don

---
+++ application/models/Chitiethoadon_model.php
@@ -0,0 +1,33 @@
+<?php
+
+class Chitiethoadon_model extends CI_Model 
+{
+	protected $table = 'Chitiethoadon';
+
+	public function getAll()
+	{
+		return $this->db->get($this->$table)->result_array();
+	}
+
+	public function getByMaCTHD($MaCTHD)
+	{
+		return $this->db->get_where($this->table, ['MaCTHD' => $MaCTHD])->row_array();
+	}
+
+	public function create($params)
+    {
+		$this->db->insert($this->table, $params);
+		return $this->db->insert_MaCTHD();
+	}
+	
+    public function update($MaCTHD, $params)
+    {
+		$this->db->where('MaCTHD', $MaCTHD);
+		return $this->db->update($this->table, $params);
+	}
+	
+    public function delete($MaCTHD)
+    {
+        return $this->db->delete($this->table, ['MaCTHD'=> $MaCTHD]);
+    }
+}
---
+++ application/models/Hoadon_model.php
@@ -0,0 +1,33 @@
+<?php
+
+class Hoadon_model extends CI_Model 
+{
+	protected $table = 'Hoadon';
+
+	public function getAll()
+	{
+		return $this->db->get($this->$table)->result_array();
+	}
+
+	public function getByMaHD($MaHD)
+	{
+		return $this->db->get_where($this->table, ['MaHD' => $MaHD])->row_array();
+	}
+
+	public function create($params)
+    {
+		$this->db->insert($this->table, $params);
+		return $this->db->insert_MaHD();
+	}
+	
+    public function update($MaHD, $params)
+    {
+		$this->db->where('MaHD', $MaHD);
+		return $this->db->update($this->table, $params);
+	}
+	
+    public function delete($MaHD)
+    {
+        return $this->db->delete($this->table, ['MaHD'=> $MaHD]);
+    }
+}
